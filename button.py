6c238f828ca26ef5371fdb9f3ab37af66927d945
Pawps <henschelogbonna@gmail.com>
2022-11-15 01:11:25 +0100

    Another commit

--- button.py
+++ button.py
@@ -1,12 +1,13 @@
 import pygame.font
 
-class Buttom:
+class Button:
+
     def __init__(self, ai_game, msg):
         """Initailize button attributes."""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
-        # Set the simensions and the properties of the button.
+        # Set the dimensions and the properties of the button.
         self.width, self.height = 200, 50
         self.button_color = (0, 255, 0)
         self.text_color = (255, 255, 255)
@@ -21,7 +22,12 @@ class Buttom:
 
     def _prep_msg(self, msg):
         """Turn msg into rendered image and center text on thr button."""
-        self.msg_image = self.font.render(msg, True,self.text_color,
+        self.msg_image = self.font.render(msg, True, self.text_color,
             self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = self.rect.centergit
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Draw blank button and then draw message.
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
--- settings.py
+++ settings.py
@@ -2,25 +2,43 @@ class Settings:
 	"""A class to store all setings for Alien Invasion."""
 	
 	def __init__(self):
-		"""Initialize the game settings."""
+		"""Initialize the game's static settings."""
 		# Screen settings
-		self.screen_width = 1280
-		self.screen_height = 650
+		self.screen_width = 1366
+		self.screen_height = 768
 		self.bg_color = (230, 230, 230)
 	
 		# Ship setting
-		self.ship_speed = 1.5
 		self.ship_limit = 3
 		
 		# Bullet settings
-		self.bullet_speed = 1.0
-		self.bullet_width = 300
+		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 3
 
 		# Alien settings
+		self.fleet_drop_speed = 10
+
+		# How quickly the game speed up.
+		self.speedup_scale = 1.1
+
+		self.initialize_dynamic_settings()
+
+	def initialize_dynamic_settings(self):
+		"""Initialize settings that change throughout the game."""
+		self.ship_speed = 1.5
+		self.bullet_speed = 3.0
 		self.alien_speed = 1.0
-		self.fleet_drop_speed = 100
+		
 		# fleet direction 1 reps right; -1 reps left.
 		self.fleet_direction = 1
+
+		# Scoring
+		self.alien_points = 1
+
+	def increase_speed(self):
+		"""Increase speed settings."""
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
